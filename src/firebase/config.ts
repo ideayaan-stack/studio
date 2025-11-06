d5e87252647596cbd6659792b6065e0817a401b1
Ideayaan <ideayaan@gmail.com>
2025-11-06 17:24:35 +0000

    chore: update Firebase configuration and SDK setup

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,13 @@
 // You do not need to edit this file.
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDHF7Z8O0RI17Crz-faOmeQRsMtBecLgGY",
+  authDomain: "studio-2788856224-eb316.firebaseapp.com",
+  projectId: "studio-2788856224-eb316",
+  storageBucket: "studio-2788856224-eb316.appspot.com",
+  messagingSenderId: "8193486209",
+  appId: "1:8193486209:web:5bc09ed9445a90cb2acc80",
+  measurementId: "G-0DQETY0TN5"
 };
 
 // Check if all required environment variables are set, but only on the client side
